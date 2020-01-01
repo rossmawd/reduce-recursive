b90d5c2766489bc9cee44561a845e620377584e0
Ross Mawdsley <rossmawd@gmail.com>
2019-12-31 16:55:38 -0800

    finding the max value in an array of objects

--- largestValue.js
+++ largestValue.js
@@ -1,11 +1,17 @@
-let maxCallback = (acc, cur) => Math.max(acc.x, cur.x)
-let maxCallback2 = (max, cur) => Math.max(max, cur)
+// let maxCallback2 = (acc, cur) => Math.max(acc.x, cur.x)
 
-// reduce() without initialValue
-[{ x: 22 }, { x: 42 }].reduce(maxCallback) // 42
-[{ x: 22 }].reduce(maxCallback) // { x: 22 }
-[].reduce(maxCallback) // TypeError
 
-// map/reduce; better solution, also works for empty or larger arrays
-[{ x: 22 }, { x: 42 }].map(el => el.x)
-  .reduce(maxCallback2, -Infinity)
+// // reduce() without initialValue
+// console.log([{ x: 22 }, { x: 42 }].reduce(maxCallback2)) // 42
+// // [{ x: 22 }].reduce(maxCallback) // { x: 22 }
+
+
+//map/reduce; better solution, also works for empty or larger arrays
+let maxCallback = (max, cur) => {
+  console.log("the accumulator is currently:", max)
+  return Math.max(max, cur.x)
+}
+
+let arr1 = [{ x: 22 }, { x: 42 }, { x: 99 }]
+let reducedToMaxValue = arr1.reduce(maxCallback, -Infinity)
+console.log("The max value is:", reducedToMaxValue)
